10a8562de9bc1a0e66f02e78ca5a3d874cd36bc1
felizardo27 <jp.felizardo27@gmail.com>
2025-03-21 09:12:59 -0300

    fix: error in type any

--- src/components/Card/components/CardContent/index.tsx
+++ src/components/Card/components/CardContent/index.tsx
@@ -1,13 +1,16 @@
-import { CardProps } from "../..";
+import { useLanguage } from "../../../../context/useLanguage";
+import { ProjectProps } from "../../../../interfaces/firebaseTypes";
 import { ContainerContent, ContainerDescription, ItemDescription, Title } from "./styles";
 
 
-export function CardContent({ titleCard, description }: Pick<CardProps, 'titleCard' | 'description'>) {
+export function CardContent({ title, description }: Pick<ProjectProps, 'title' | 'description'>) {
+  const { language } = useLanguage();
+  const descriptionText = language === 'enUs' ? description.en : description.pt;
   return (
     <ContainerContent>
-      <Title>{titleCard}</Title>
+      <Title>{title}</Title>
       <ContainerDescription>
-        {description.map((item, index) => (
+        {Object.entries(descriptionText).map(([index, item]) => (
           <ItemDescription key={index}>
             {item}
           </ItemDescription>
